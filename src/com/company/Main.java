32cd71dfcfd1e58e20b090cf8ccea9a24b7bc75e
Алексей Дорофеев <aleksey.dorofeev@rmg-region.ru>
2024-12-14 14:19:46 +0300

    attempt 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,45 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Задание 1");
+
+        int[] costPerMonth = {501, 1000, 2536, 300, 720};
+        int sumCost = 0;
+
+        for (int j : costPerMonth) {
+            sumCost += j;
+        }
+        System.out.println("Сумма трат за месяц составила " + sumCost + " рублей");
+
+        System.out.println("Задание 2");
+
+        int maxCost = costPerMonth[1];
+        int minCost = costPerMonth[2];
+        for (int j : costPerMonth) {
+            if (maxCost < j) {
+                maxCost = j;
+            } else if (minCost > j) {
+                minCost = j;
+            }
+        }
+        System.out.println("Минимальная сумма трат за неделю составила " + minCost + " рублей. Максимальная сумма трат за неделю составила " +
+                maxCost + " рублей");
+        System.out.println(maxCost);
+
+
+        System.out.println("Задание 3");
+
+        System.out.println("Средняя сумма трат за месяц составила " + (double) maxCost / costPerMonth.length + " рублей");
+
+        System.out.println("Задание 4");
+
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        char[] reverseReverse = new char[reverseFullName.length];
+
+        for (int i = 0; i < reverseFullName.length; i++) {
+            reverseReverse[i] = reverseFullName[reverseFullName.length - 1 -i];
+            System.out.print(reverseReverse[i]);
+        }
+
     }
 }
